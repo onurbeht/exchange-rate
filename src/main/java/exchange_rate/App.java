f5ea2679a6044ce482829700f22361677560306d
onurbeht <brunnobatista25@gmail.com>
2024-09-18 16:48:21 -0300

    First commit

---
+++ src/main/java/exchange_rate/App.java
@@ -0,0 +1,13 @@
+package exchange_rate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
